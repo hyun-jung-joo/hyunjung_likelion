01150a05935c4c2723d89c059dac64fd379f0ad1
hyun-jung-joo <wnguswjd0418@naver.com>
2023-05-04 16:47:49 +0900

    second commit

--- django_myproject/config/urls.py
+++ django_myproject/config/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='index'),
+    path('lists/', include('list.urls')),
+    path('contact/', include('contact.urls')),
 ]
---
+++ django_myproject/list/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from list import views
+
+urlpatterns = [
+    path('', views.listsHome, name='lists'),
+]
