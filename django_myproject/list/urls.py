6d9816bb61ee46fb88b979b383e2ec3634b22c77
hyun-jung-joo <wnguswjd0418@naver.com>
2023-05-12 12:49:40 +0900

    myproject update

--- django_myproject/list/urls.py
+++ django_myproject/list/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from list import views
+from .views import class_view
 
 urlpatterns = [
     path('', views.listsHome, name='lists'),
     path('create/', views.create, name='create'),
+    path('show/', class_view.as_view())
 ]
--- django_myproject/list/views.py
+++ django_myproject/list/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import redirect, render
 from .forms import PostModelForm
+from django.views.generic import ListView
+
+from .models import List
 
 # Create your views here.
 def listsHome(request):
 	return render(request, 'lists.html')
 
 def create(request) : 
+    print(f'request.method: {request.method}')
     if request.method == 'POST':
         form = PostModelForm(request.POST) # 입력 내용을 DB에 저장
+        print(f'request.POST: {request.POST}')
         if form.is_valid():  
             form.save() 
             return redirect('lists') 
     else:
         form = PostModelForm() 
     return render(request, 'form_create.html', {'form':form})
+
+class class_view(ListView) :
+    model = List # Post 기반 모델
+    ordering = ['id'] # orderby랑 같은거
+    template_name = 'show_list.html' # 출력 창
